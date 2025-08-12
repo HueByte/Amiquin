22608cdbc2224cdeb1f7e7609ab3d76c15590f82
HueByte <ihuebyte@gmail.com>
2025-08-12 03:16:56 +0200

    feat: Add ChatSession Name column to MySql and Sqlite migrations

--- source/Migrations/Amiquin.MySql/Migrations/20250812003231_AddChatSessionName_MySql.cs
+++ source/Migrations/Amiquin.MySql/Migrations/20250812003231_AddChatSessionName_MySql.cs
@@ -10,13 +10,22 @@ namespace Amiquin.MySql.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ChatSessions",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "Default Session")
+                .Annotation("MySql:CharSet", "utf8mb4");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ChatSessions");
         }
     }
 }
